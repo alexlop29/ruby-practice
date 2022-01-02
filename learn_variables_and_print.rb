80778f491874a5164b676210f4bdb2660d987fa1
Alexander Lopez <34544005+alexlop29@users.noreply.github.com>
2022-01-02 01:38:05 -0500

    Create learn_variables_and_print.rb

---
+++ learn_variables_and_print.rb
@@ -0,0 +1,13 @@
+# Define Variables
+
+name = "Alexander"
+puts name # puts - print statement
+
+# Array - List
+address = ["123", "Downtown", "NY", "11133"]
+puts address
+
+# Puts returns a nil value
+puts "Alexander"
+# p returns a value
+p "Ally"
