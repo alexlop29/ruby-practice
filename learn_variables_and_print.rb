00952cf2b1982afcba7c41c0a73b989df3126cb2
Alexander Lopez <34544005+alexlop29@users.noreply.github.com>
2022-01-03 19:11:41 -0500

    Update learn_variables_and_print.rb

--- learn_variables_and_print.rb
+++ learn_variables_and_print.rb
@@ -32,3 +32,16 @@ irb(main):011:0> p "Alexander"
 => "Alexander"
 irb(main):012:0> @Alexander
 => nil
+
+# EXAMPLE 2: DIFFERENCE BETWEEN 'puts' AND 'p'
+# 'p' RETURNS AN ARRAY IN CODE FORM (ORIGINAL FORMAT)
+# 'puts' RETURNS AN ARRAY SEPERATED BY A NEW LINE
+irb(main):013:0> p [1,2,3,4]
+[1, 2, 3, 4]
+=> [1, 2, 3, 4]
+irb(main):014:0> puts [1,2,3,4]
+1
+2
+3
+4
+=> nil
