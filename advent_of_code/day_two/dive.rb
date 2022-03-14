4d8653abe2e7df4c95d376a389557e5536ac6060
Alexander Lopez <alexander.lopez@owasp.org>
2022-03-14 17:09:38 -0400

    Day 2 - Advent of Code

---
+++ advent_of_code/day_two/dive.rb
@@ -0,0 +1,23 @@
+DIRECTORY = File.expand_path(File.dirname(__FILE__))
+SEA_LEVEL_REPORT = File.join(DIRECTORY,'02.txt')
+SEA_LEVEL_DATA = File.read(SEA_LEVEL_REPORT)
+SPLIT_SEA_LEVEL_DATA = SEA_LEVEL_DATA.split("\n")
+
+horizontal_position=0
+depth=0
+
+SPLIT_SEA_LEVEL_DATA.each_with_index { | data, index |
+    direction_by_change = data.split(" ")
+    if direction_by_change[0] == "forward"
+        horizontal_position += direction_by_change[1].to_i
+    elsif direction_by_change[0] == "up"
+        depth -= direction_by_change[1].to_i
+    else
+        depth += direction_by_change[1].to_i
+    end
+}
+
+final_answer=horizontal_position*depth
+
+puts "#{final_answer}"
+
