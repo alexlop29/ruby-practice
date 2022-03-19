3dfa27dd8266482ccfe743bfe32b7390db25e2fc
Alexander Lopez <alexander.lopez@owasp.org>
2022-03-19 15:39:07 -0400

    Update 01_sandiwch_spec.rb

--- rspec/spec/01_sandwich_spec.rb
+++ rspec/spec/01_sandwich_spec.rb
@@ -1,9 +1,12 @@
-Rspec.describe 'An ideal sandwich' do
+RSpec.describe 'An ideal sandwich' do
     it 'is delicious' do
+        # Arrange
         sandwich = Sandwich.new('delicious',[])
 
+        # Act
         taste = sandwich.taste
 
+        # Assert
         expect(taste).to eq('delicious')
     end
 end
