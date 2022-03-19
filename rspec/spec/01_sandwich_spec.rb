573558d5123aa44b07be4414e743b2642979f390
Alexander Lopez <alexander.lopez@owasp.org>
2022-03-19 17:44:34 -0400

    Run bundle exec rubocop -aA spec/01_sandwich_spec.rb

--- rspec/spec/01_sandwich_spec.rb
+++ rspec/spec/01_sandwich_spec.rb
@@ -1,30 +1,29 @@
+# frozen_string_literal: true
+
 # NOTE: (alopez) Defining the structure is usually done in another
 # directory (e.g. /lib)
 # https://ruby-doc.org/core-3.1.1/Struct.html
 Sandwich = Struct.new(:taste, :toppings)
 
-
 RSpec.describe 'An ideal sandwich' do
-    before { @sandwich = Sandwich.new('delicious', [) } 
-    it 'is delicious' do
-        # Arrange
-        sandwich = Sandwich.new('delicious',[])
+  before { @sandwich = Sandwich.new('delicious', []) }
+  it 'is delicious' do
+    # Arrange
+    sandwich = Sandwich.new('delicious', [])
 
-        # Act
-        taste = sandwich.taste
+    # Act
+    taste = sandwich.taste
 
-        # Assert
-        expect(taste).to eq('delicious')
-    end
+    # Assert
+    expect(taste).to eq('delicious')
+  end
 
-    it 'lets me add toppings' do
-        sandwich = Sandwich.new('delicious',[])
+  it 'lets me add toppings' do
+    sandwich = Sandwich.new('delicious', [])
 
-        sandwich.toppings << 'cheese'
-        toppings = sandwich.toppings
+    sandwich.toppings << 'cheese'
+    toppings = sandwich.toppings
 
-        expect(toppings).not_to be_empty
-
-    end    
+    expect(toppings).not_to be_empty
+  end
 end
-
