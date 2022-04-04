2dfa1cdefff7d2ed1b5e499a30b1eb45d13e43ae
Alexander Lopez <alexander.lopez@owasp.org>
2022-04-03 22:54:55 -0400

    Update Gemfile, Begin Spec for Advent of Code Challenges

---
+++ advent_of_code/spec/01_spec.rb
@@ -0,0 +1,26 @@
+
+RSpec.describe 'Sonar Sweep' do
+  let(:input) do
+    <<~TXT
+    199
+    200
+    208
+    210
+    200
+    207
+    240
+    269
+    260
+    263
+    TXT
+  end
+  
+  it 'counts the number of times a depth measurement' do
+    solution = 
+    
+    expect 
+  end
+
+end
+
+
