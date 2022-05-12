f4418066a532dd94b30bfe1ecd4d48961101d626
Alexander Lopez <alexander.lopez@owasp.org>
2022-05-12 16:01:59 -0400

    Rubocop on 01.rb

--- advent_of_code/lib/01.rb
+++ advent_of_code/lib/01.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 def depth_measurement_increase(input)
   split_sea_level_data = input.split
   int_sea_level_data = split_sea_level_data.map(&:to_i)
@@ -6,36 +8,31 @@ def depth_measurement_increase(input)
 
   int_sea_level_data.each_with_index do |data, index|
     break if index == int_sea_level_data.size - 1
-  
+
     upcoming_data_point = int_sea_level_data[index + 1]
-  
+
     counter += 1 if data < upcoming_data_point
   end
 
   puts counter
-
-  return counter
 end
 
 def sliding_window_increase(input)
   split_sea_level_data = input.split
   int_sea_level_data = split_sea_level_data.map(&:to_i)
 
-  counter = 0 
+  counter = 0
 
   int_sea_level_data.each_with_index do |_data, index|
     break if index == int_sea_level_data.size - 3
-  
+
     first_window_sum = int_sea_level_data[index] + int_sea_level_data[index + 1] + int_sea_level_data[index + 2]
     second_window_sum = int_sea_level_data[index + 1] + int_sea_level_data[index + 2] + int_sea_level_data[index + 3]
-  
+
     counter += 1 if first_window_sum < second_window_sum
   end
 
   puts counter
-
-  return counter
-
 end
 
 DIRECTORY = __dir__
