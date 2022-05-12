b20b12417c1286491279120a1a1550bca0f36a0b
Alexander Lopez <alexander.lopez@owasp.org>
2022-05-12 18:06:59 -0400

    Clean up 02.rb

--- advent_of_code/spec/02_spec.rb
+++ advent_of_code/spec/02_spec.rb
@@ -21,8 +21,4 @@ RSpec.describe 'dive' do
   it 'multiplies horizontal position by depth' do
     expect(multiply_position_and_depth(input)).to eq(150)
   end
-
-#   it 'multiplies horizontal position by depth' do
-#     expect(multiply_position_and_depth(input)).to eq(150)
-#   end
 end
