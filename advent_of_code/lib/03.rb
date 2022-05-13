a38b9d7e9030fbab823fa7fa843d801a8d702000
Alexander Lopez <alexander.lopez@owasp.org>
2022-05-13 14:25:11 -0400

    Finish Part 1 Day 3

--- advent_of_code/lib/03.rb
+++ advent_of_code/lib/03.rb
@@ -1,12 +1,42 @@
 def calculate_gamma_rate(input)
-  count_of_zeroes = 0
-  count_of_ones = 0
   gamma_rate=""
   data = input.split("\n")
-  data.each do |num|
-    binary = num.split("")
-    binary.each do { |curr_char| curr_char = "0" ? count_of_zeroes+=1 : count_of_ones+=1 }
+  for i in 0..data[0].length - 1
+    count_of_zeroes = 0
+    count_of_ones = 0
+    data.each do |num|
+      binary = num.split("")
+      binary[i] == "0" ? count_of_zeroes+=1 : count_of_ones+=1
+    end
+    count_of_zeroes > count_of_ones ? gamma_rate.concat("0") : gamma_rate.concat("1")
   end
+  return gamma_rate
 end
 
-# Consider looping by the length of the strings (First; they should all be the same), inner loop to grab each number at that char; use ternary operator to compare
+def calculate_epsilon_rate(input)
+  epsilon_rate=""
+  data = input.split("\n")
+  for i in 0..data[0].length - 1
+    count_of_zeroes = 0
+    count_of_ones = 0
+    data.each do |num|
+      binary = num.split("")
+      binary[i] == "0" ? count_of_zeroes+=1 : count_of_ones+=1
+    end
+    count_of_zeroes < count_of_ones ? epsilon_rate.concat("0") : epsilon_rate.concat("1")
+  end
+  return epsilon_rate
+end
+
+def calculate_power_consumption(input)
+  gamma_rate = calculate_gamma_rate(input).to_i(2)
+  epsilon_rate = calculate_epsilon_rate(input).to_i(2)
+  # puts gamma_rate * epsilon_rate 
+  return gamma_rate * epsilon_rate
+end
+
+# DIRECTORY = __dir__
+# DATA = File.join(DIRECTORY, '../inputs/03.txt')
+# READ_DATA = File.read(DATA)
+
+# calculate_power_consumption(READ_DATA)
--- advent_of_code/spec/03_spec.rb
+++ advent_of_code/spec/03_spec.rb
@@ -24,16 +24,11 @@ RSpec.describe 'binary diagnostic' do
     expect(calculate_gamma_rate(input)).to match("10110")
   end
 
-#   it 'calculates the epsilon rate in binary' do
-#     expect(calculate_epsilon_rate(input)).to eq(01001)
-#   end
+  it 'calculates the epsilon rate in binary' do
+    expect(calculate_epsilon_rate(input)).to match("01001")
+  end
 
-#   it 'converts binary to decimal' do
-#     expect(convert_binary_to_decimal(01001)).to eq(9)
-#     expect(convert_binary_to_decimal(10110)).to eq(22)
-#   end
-
-#   it 'calculates the power consumption in decimal' do
-#     expect(calculate_power_consumption(input)).to eq(198)
-#   end
+  it 'calculates the power consumption in decimal' do
+    expect(calculate_power_consumption(input)).to eq(198)
+  end
 end
